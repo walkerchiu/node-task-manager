16022a9e3a6b87ddce1ced1884d78b40389d8fec
walkerchiu <chenjen.chiou@gmail.com>
2022-01-15 17:00:38 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,169 @@
+const { EventEmitter } = require('events');
+
+// Represents a task execution with a task ID and service name
+class TaskExecution {
+    constructor(taskId, service) {
+        this.taskId = taskId;
+        this.service = service;
+    }
+}
+
+// Enum for execution status
+class ExecutionStatusEnum {}
+ExecutionStatusEnum.SUCCESS = 'SUCCESS';
+ExecutionStatusEnum.FAILED = 'FAILED';
+
+// Task package A
+class TaskPackageA {
+    constructor() {
+        this.name = 'PackageA';
+        this.description = '';
+        this.version = '0.1.0';
+    }
+
+    async run() {
+        // Implement TaskPackageA's execution logic
+        let result = true; // Placeholder logic, actual logic should be implemented
+        return result;
+    }
+
+    async pause() {
+        // Implement TaskPackageA's pause logic
+    }
+
+    async resume() {
+        // Implement TaskPackageA's resume logic
+    }
+
+    async abort() {
+        // Implement TaskPackageA's abort logic
+    }
+
+    async revert(seconds) {
+        // Implement TaskPackageA's revert logic
+    }
+}
+
+// Task manager for managing background tasks
+class TaskManager extends EventEmitter {
+    constructor() {
+        super();
+        this.taskTimers = {};
+        this.taskQueue = [];
+        this.taskPackageA = new TaskPackageA();
+        console.log("TaskManager initialized...");
+        this.isRunning = false;
+        this.interval = null;
+    }
+
+    async run() {
+        this.isRunning = true;
+        while (this.isRunning) {
+            if (this.taskQueue.length > 0) {
+                let taskExecution = await this.dequeueTaskExecution();
+                if (taskExecution) {
+                    console.log(`Dequeue Task: ${taskExecution.taskId}, Service: ${taskExecution.service}`);
+                    let success = await this.processTaskExecution(taskExecution);
+                    if (success) {
+                        await this.updateExecutionStatus(taskExecution, ExecutionStatusEnum.SUCCESS);
+                    } else {
+                        await this.updateExecutionStatus(taskExecution, ExecutionStatusEnum.FAILED);
+                    }
+                }
+            }
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+    }
+
+    async processTaskExecution(taskExecution) {
+        let taskId = taskExecution.taskId;
+        if (taskExecution.service === 'task_package_a') {
+            console.log("TaskManager executing PackageA...");
+            return await this.taskPackageA.run();
+        }
+        return false;
+    }
+
+    async updateExecutionStatus(taskExecution, status) {
+        // Update execution status to database or other storage
+    }
+
+    async enqueueTask(taskId, service) {
+        this.taskQueue.push(new TaskExecution(taskId, service));
+        console.log(`Enqueue Task: ${taskId}, Service: ${service}`);
+    }
+
+    async dequeueTaskExecution() {
+        if (this.taskQueue.length > 0) {
+            return this.taskQueue.shift();
+        }
+        return null;
+    }
+
+    async startTimer(tqId, timeout) {
+        if (tqId in this.taskTimers) {
+            return;
+        }
+        this.taskTimers[tqId] = setTimeout(async () => {
+            console.log(`Task ${tqId} timed out`);
+            await this.retryTask(tqId);
+            delete this.taskTimers[tqId];
+        }, timeout);
+    }
+
+    async retryTask(tqId) {
+        // Re-queue task for execution
+    }
+
+    async startMonitoring() {
+        this.interval = setInterval(() => {
+            if (this.taskQueue.length === 0 && Object.keys(this.taskTimers).length === 0) {
+                console.log("All tasks completed, restarting TaskManager...");
+                this.restart();
+            }
+        }, 1000); // Check TaskManager status every second
+    }
+
+    stopMonitoring() {
+        clearInterval(this.interval);
+    }
+
+    restart() {
+        this.stopMonitoring();
+        this.isRunning = false;
+        this.run();
+    }
+}
+
+async function main() {
+    let taskManager = new TaskManager();
+
+    // Start TaskManager execution
+    taskManager.run();
+
+    // Simulate task queue
+    let tasks = [
+        { taskId: Math.floor(Math.random() * 1000), service: 'task_package_a' },
+        { taskId: Math.floor(Math.random() * 1500), service: 'task_package_a' },
+        { taskId: Math.floor(Math.random() * 1500), service: 'task_package_a' },
+        { taskId: Math.floor(Math.random() * 1500), service: 'task_package_a' },
+        // Add more tasks as needed
+    ];
+
+    async function addTasks(tasks) {
+        for (let task of tasks) {
+            taskManager.enqueueTask(task.taskId, task.service);
+            let wait_seconds = Math.random() * 1000;
+            await new Promise(resolve => setTimeout(resolve, wait_seconds));
+        }
+    }
+
+    await addTasks(tasks);
+
+    // Start monitoring TaskManager execution
+    setTimeout(() => {
+        taskManager.startMonitoring();
+    }, 1000); // Wait for 1 second before starting to monitor the task queue
+}
+
+main();
